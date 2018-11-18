3ca7f3bc1026efce656beb899bc0abcad07f9707
Kishore Venkateshan <kishorevenkateshan@Kishores-MacBook-Pro.local>
2018-11-17 18:58:11 -0800

    Checking pure loss

--- model.py
+++ model.py
@@ -51,12 +51,9 @@ def _get_loss_(prediction, truth):
     non_void_truth = tf.boolean_mask(tensor=truth, mask=ignore_void_mask, name='NonVoidTruth')
     non_void_prediction = tf.boolean_mask(tensor=prediction, mask=ignore_void_mask, name='NonVoidPrediction')
 
-    loss = non_void_truth
-    """
-    tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.cast(non_void_truth, tf.float64),
-                                                   logits=non_void_prediction,
+    loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=non_void_truth,
+                                                   logits=non_void_truth,
                                                    name='SigmoidLoss')
-    """
     return loss
 
 
