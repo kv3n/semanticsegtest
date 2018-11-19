790623b70ee9cb9e47f34fbae1a287132eab3b41
Kishore Venkateshan <kishorevenkateshan@Kishores-MacBook-Pro.local>
2018-11-18 21:48:52 -0800

    Minor fix

--- model.py
+++ model.py
@@ -91,8 +91,7 @@ def build_model(image_batch, true_segmentation):
 
     # Deconvolution Layer
     output = _create_deconv_layer_(name='DeConv1', inputs=image_batch, size=64, stride=32, filters=1)
-    output = tf.nn.sigmoid()
-
+    
     loss = _get_loss_(prediction=output, truth=true_segmentation)
     optimize = tf.train.MomentumOptimizer(learning_rate=LEARNING_RATE, momentum=MOMENTUM).minimize(loss=loss)
 
