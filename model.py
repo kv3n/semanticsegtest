65ac579b8706e2ec0b6becefa92865fa4c2e9967
Kishore Venkateshan <kishorevenkateshan@Kishores-MacBook-Pro.local>
2018-11-19 14:46:13 -0800

    Adjusted activation

--- model.py
+++ model.py
@@ -26,7 +26,7 @@ def _create_deconv_layer_(name, inputs, filters, size=5, stride=1, padding='same
                                       filters=filters,
                                       kernel_size=[size, size],
                                       strides=[stride, stride],
-                                      activation=None,
+                                      activation=tf.nn.sigmoid,
                                       padding=padding,
                                       name=layer_name,
                                       bias_initializer=tf.contrib.layers.xavier_initializer(),
@@ -52,7 +52,7 @@ def _get_loss_(prediction, truth):
     non_void_prediction = tf.gather_nd(params=prediction, indices=ignore_void_mask,
                                        name='NonVoidPrediction')
 
-    loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=non_void_truth, logits=non_void_prediction)
+    loss = tf.losses.mean_squared_error(labels=non_void_truth, predictions=non_void_prediction)
 
     return loss
 
